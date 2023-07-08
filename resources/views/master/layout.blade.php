f2d984e9c8776a22be3b4fd99fb82a9be334e831
ilhmstxr <ilhambintanghuh30332@gmail.com>
2023-07-08 21:47:34 +0700

    fixing

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -1,60 +1,66 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-        
-        <!--CSS LAINNYA-->
-        <style>
-            .jumbotron{
-                padding: 6rem 1rem;
-                background: #e2edff;
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
-            }
-            .nav-link{
-                color: white;
-            }
+    <!--CSS LAINNYA-->
+    <style>
+        .jumbotron {
+            padding: 6rem 1rem;
+            background: #e2edff;
 
-            #projects{
-                background: #e2edff;
-            }
+        }
 
-            section{
-                padding-top: 5rem;
-            }
+        .nav-link {
+            color: white;
+        }
 
-            .container2 {
-                width: 100%;
-            }
-        </style>
+        #projects {
+            background: #e2edff;
+        }
+
+        section {
+            padding-top: 5rem;
+        }
+
+        .container2 {
+            width: 100%;
+        }
+    </style>
     <title>Muhammad Tsany Fulviansyah | @yield('title')</title>
 </head>
+
 <body>
     @section('navbar')
-    <!--Navbar-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm fixed-top">
-        <div class="container">
-        <a class="navbar-brand" href="/">Muhammad Tsany Fulviansyah</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkaria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav ms-auto">
-                <a class="nav-link active" href="/">Home</a>
-                <a class="nav-link active" href="/about">About</a>
-                <a class="nav-link active" href="/projects">Projects</a>
-                <a class="nav-link active" href="/contact">Contact</a>
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="/">Muhammad Tsany Fulviansyah</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkaria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ms-auto">
+                        <a class="nav-link active" href="/">Home</a>
+                        <a class="nav-link active" href="/about">About</a>
+                        <a class="nav-link active" href="/projects">Projects</a>
+                        <a class="nav-link active" href="/contact">Contact</a>
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
-    </nav>
-    <!--End of navbar--> 
+        </nav>
+        <!--End of navbar-->
     @show
 
     <div class="container2">
@@ -62,12 +68,13 @@
     </div>
 
     @section('footer')
-    <!--Footer-->
-    <footer class="bg-primary text-white text-center p-3">
-        <p>Made by <a href="#" class="text-white fw-bold">Muhammad Tsany Fulviansyah</a></p>
-    </footer>
-    <!--End of footer-->
+        <!--Footer-->
+        <footer class="bg-primary text-white text-center p-3">
+            <p>Made by <a href="#" class="text-white fw-bold">Muhammad Tsany Fulviansyah</a></p>
+        </footer>
+        <!--End of footer-->
     @show
 
 </body>
+
 </html>
