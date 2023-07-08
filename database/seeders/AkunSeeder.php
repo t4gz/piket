e2a9d9fa422555bbb1eb165694d40986d25b6328
t4gz <fulviansyah787@gmail.com>
2023-07-08 21:07:00 +0800

    ngisi seeders kelas kelas ;3

---
+++ database/seeders/AkunSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AkunSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = [
+            [
+                'username'=>'ilhxmstar',
+                'email'=>'ilhxm@gmail.com',
+                'roles_id'=>1,
+                'password'=>bcrypt('123'),
+            ],
+            [
+                'username'=>'tsanny',
+                'email'=>'tagz@gmail.com',
+                'roles_id'=>1,
+                'password'=>bcrypt('123'),
+            ],
+            [
+                'username'=>'steven',
+                'email'=>'steven@gmail.com',
+                'roles_id'=>1,
+                'password'=>bcrypt('123'),
+            ],
+        ];
+
+        User::insert($admin);
+    }
+    }
+}
